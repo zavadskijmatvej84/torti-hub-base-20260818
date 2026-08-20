641588408fe0e6faf411584ddc5ea5a87ca0e3c4
Torti <zavadskijmatvej84@gmail.com>
2026-08-20 22:03:04 +0300

    Restore MM2 values alongside rubles

--- main.part1.lua
+++ main.part1.lua
@@ -40,7 +40,7 @@ function FormatCatalogValue(v)
 	if not numeric then
 		return tostring(v)
 	end
-	return string.format("%.2f RUB ₽", numeric)
+	return string.format("%s | %.2f₽", FormatValue(numeric), numeric)
 end
 
 function NormalizeItemName(value)
